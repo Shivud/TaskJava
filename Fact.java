3eb47ddec3c842f2772aa58c5435b5b91aedc5a4
shivanand.d@khyath.com <shivanand.d@khyath.com>
2024-08-29 15:57:22 +0530

    commiting java factorial

--- Fact.java
+++ Fact.java
@@ -1,11 +1,12 @@
 public class Fact{
     public static void main(String[]args){
-        int fact =1;
-        int n=3;
+       int i,fact=1;
+        int num=5;
 
-        for(int i=1;i<=n;i++)
+        for(int i=1;i <=5;i++)
         {
             fact=fact*i;
         }
+        System.out.println("factorial"+num+ "is"+fact);
     }
 }
