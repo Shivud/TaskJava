870323e60c5091803a2b2ba42b44cbb4eccf3aa5
shivanand.d@khyath.com <shivanand.d@khyath.com>
2024-08-29 15:41:14 +0530

    factorial programme

---
+++ Fact.java
@@ -0,0 +1,11 @@
+public class Fact{
+    public static void main(String[]args){
+        int fact =1;
+        int n=3;
+
+        for(int i=1;i<=n;i++)
+        {
+            fact=fact*i;
+        }
+    }
+}
